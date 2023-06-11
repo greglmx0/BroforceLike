3f9d31ccf8f6c050f2dc3ccd50ef829bf5268b2d
UDeficit <gergoire.lamoureux@epitech.eu>
2023-06-11 21:12:57 +0200

    [ADD] game scren and player

--- entity/Player.lua
+++ entity/Player.lua
@@ -1,32 +1,56 @@
-local love = require "love"
+function Player(num_lives)
 
-player = {}
-player.__index = player
+    return {
+        lives = num_lives or 3,
+        x = love.graphics.getWidth() / 2,
+        y = love.graphics.getHeight() / 2,
+        rotation = 0,
+        expload_time = 0,
+        exploading = false,
+        thrusting = false,
+        lazers = {},
+        wapon = {},
 
-function player:new(name, x, y)
-    local p = {}
-    setmetatable(p, player)
 
-    p.name = name
-    p.x = x
-    p.y = y
+        draw = function(self)
 
-    print("Player " .. p.name .. " created !")
-    print("x: " .. p.x .. " y: " .. p.y)
-
-    return p
+        end
+    }
 end
 
-function player:draw()
-    -- love.graphics.setColor(125, 125, 125)
-    -- love.graphics.rectangle("fill", self.x, self.y, 100, 100)
-    -- love.graphics.circle("fill", self.x, self.y, 50)
-end
+return Player
 
-function player:update(dt)
-    -- self.x = self.x + 1
-end
 
-function player:mousepressed(x, y, button, istouch)
-    print("PAN !")
-end
+
+
+
+--player = {}
+--player.__index = player
+--
+--function player:new(name, x, y)
+--    local p = {}
+--    setmetatable(p, player)
+--
+--    p.name = name
+--    p.x = x
+--    p.y = y
+--
+--    print("Player " .. p.name .. " created !")
+--    print("x: " .. p.x .. " y: " .. p.y)
+--
+--    return p
+--end
+--
+--function player:draw()
+--    -- love.graphics.setColor(125, 125, 125)
+--    -- love.graphics.rectangle("fill", self.x, self.y, 100, 100)
+--    -- love.graphics.circle("fill", self.x, self.y, 50)
+--end
+--
+--function player:update(dt)
+--    -- self.x = self.x + 1
+--end
+--
+--function player:mousepressed(x, y, button, istouch)
+--    print("PAN !")
+--end
--- scenes/GameIntro.lua
+++ scenes/GameIntro.lua
@@ -1,32 +1,111 @@
+local love = require("love")
+local Text = require "../components/Text"
 
-local intro = {}
+function Game(func, difficulty, lives, level)
+    local btn_text = {}
+    func = func or function()
+        print("This button has no function attached")
+    end
 
-function intro.load()
-    love.graphics.setBackgroundColor(0, 0, 0)
-    love.graphics.setColor(255, 255, 255)
-    love.graphics.setNewFont(12)
+    if text_y then
+        btn_text.y = text_y + button_y
+    else
+        btn_text.y = button_y
+    end
 
-    -- player = player:new("Mavin",100, 100)
+    if text_x then
+        btn_text.x = text_x + button_x
+    else
+        btn_text.x = button_x
+    end
+    return {
+
+        difficulty = difficulty or 1,
+        score = 0,
+        lives = lives or 3,
+        level = level or 1,
+        state = {
+            menu = true,
+            paused = false,
+            running = false,
+            ended = false,
+        },
+
+        changeGameState = function(self, state)
+            game.state["menu"] = state == "menu"
+            game.state["paused"] = state == "paused"
+            game.state["running"] = state == "running"
+            game.state["ended"] = state == "ended"
+        end,
+
+
+        startNewGame = function (self, player)
+
+            self:changeGameState("running")
+
+        end,
+
+        setButtonColor = function(self, red, green, blue)
+            self.button_color = { r = red, g = green, b = blue }
+        end,
+
+        setTextColor = function(self, red, green, blue)
+            self.text_color = { r = red, g = green, b = blue }
+        end,
+
+        checkHover = function(self, mouse_x, mouse_y, cursor_radius)
+            if (mouse_x + cursor_radius >= self.button_x) and (mouse_x - cursor_radius <= self.button_x + self.width) then
+                if (mouse_y + cursor_radius >= self.button_y) and (mouse_y - cursor_radius <= self.button_y + self.height) then
+                    return true
+                end
+            end
+
+            return false
+        end,
+
+        click = function(self)
+            func()
+        end,
+
+        draw = function(self)
+
+            love.graphics.setColor(120, 120, 120)
+            love.graphics.rectangle("fill", self .button_x, self.button_y, self.width, self.height)
+
+        end
+
+    }
+
 end
 
-function intro.draw()
+return Game
 
-    love.graphics.setColor(0, 0, 0)
-    love.graphics.print("Intro", 10, 10)
-    -- player:draw()
-end
-
-function intro.mousepressed(x, y, button, istouch)
-    --if button == 1 then
-    --    if x >= self.x and x <= self.x + self.width and y >= self.y and y <= self.y + self.height then
-    --
-    --        player:mousepressed(x, y, button, istouch)
-    --    end
-    --end
-end
-
-function intro.update(dt)
-    --player:update(dt)
-end
-
-return intro
+--function intro.load()
+--    love.graphics.setBackgroundColor(0, 0, 0)
+--    love.graphics.setColor(255, 255, 255)
+--    love.graphics.setNewFont(12)
+--
+--    -- player = player:new("Mavin",100, 100)
+--end
+--
+--function intro.draw()
+--
+--    love.graphics.setColor(0, 0, 0)
+--    love.graphics.print("Intro", 10, 10)
+--    -- player:draw()
+--end
+--
+--function intro.mousepressed(x, y, button, istouch)
+--    --if button == 1 then
+--    --    if x >= self.x and x <= self.x + self.width and y >= self.y and y <= self.y + self.height then
+--    --
+--    --        player:mousepressed(x, y, button, istouch)
+--    --    end
+--    --end
+--end
+--
+--function intro.update(dt)
+--    --player:update(dt)
+--end
+--
+--return intro
